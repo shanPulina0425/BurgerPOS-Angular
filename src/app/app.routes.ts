9e1212d0f1cfc530d714212281a04af5b6bf8379
shanPulina0425 <shangtav@gmail.com>
2026-02-19 16:52:57 +0530

    feat:add menu component and update routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { OrderComponent } from './components/order/order.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 export const routes: Routes = [
 
     {path:'',component:HomeComponent},
     {path:'navHome',component:HomeComponent},
-    {path:'navOrder',component:OrderComponent}
+    {path:'navOrder',component:OrderComponent},
+    {path:'navMenu',component:MenuComponent}
+    
+    
 
 ];
